5e1a04811211cbf627d65f1627c7e0ee55d8edc9
prathmeshdagadelsp-cell <prathmeshdagadelsp@gmail.com>
2026-02-07 20:49:53 +0530

    Add files via upload

---
+++ Assignment_8/assignment8-4/main.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+ # include<add.h>
+
+int main ()
+{
+    int A  = 0 ; 
+    int B = 0 ; 
+    int Ans = 0 ;
+
+    scanf("%d%d",&A,&B) ;
+
+
+    Ans = add(A,B) ;
+
+    printf("Addition is %d\n",Ans) ;
+
+    return 0 ; 
+}
